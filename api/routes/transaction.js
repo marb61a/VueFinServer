95317b941d46d2e0b514f4c8f9ac40cc781ca2ad
marb61a <marb61a@gmail.com>
2018-08-29 16:34:55 +0100

    added get Transaction running balance route

--- api/routes/transaction.js
+++ api/routes/transaction.js
@@ -16,5 +16,39 @@ module.exports = function (router) {
         $lt: endDt
       }
     }
+
+    Transaction.find(qry)
+      .sort({ 'transactionDate': 1 })
+      .exec()
+      .then(docs => res.status(200).json(docs))
+      .catch(err => res.status(500)
+        .json({
+          message: 'Error finding user transactions',
+          error: err
+        })
+      )
+  })
+
+  // Get the running transactions balance for a user
+  router.get('/transaction/balance/:year/:month', function (req, res) {
+    const userId = req.get('userId')
+    const month = req.params.month - 1
+    const year = req.params.year
+    const endDt = new Date(Date.UTC(year, month, 1))
+    const pipeline = [
+      {
+        $match: {
+          userId: userId,
+          transactionDate: { $lt: endDt }
+        }
+      },
+      {
+        $group: {
+          _id: null,
+          charges: { $sum: '$charge' },
+          deposits: { $sum: '$deposit' }
+        }
+      }
+    ]
   })
 }
