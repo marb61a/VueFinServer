2e115d73971cf55a1c704df73ddb39e5361d007f
marb61a <marb61a@gmail.com>
2018-08-29 12:58:24 +0100

    added get, post and put routes to user

--- api/routes/user.js
+++ api/routes/user.js
@@ -11,4 +11,29 @@ module.exports = function (router) {
         })
       )
   })
+
+  router.get('/user/email/:email', function (req, res) {
+    User.find({ 'email': req.params.email }).exec()
+      .then(docs => res.status(200).json(docs))
+      .catch(err => res.status(500)
+        .json({
+          message: 'Error finding user',
+          error: err
+        })
+      )
+  })
+
+  router.post('/user', function (req, res) {
+    let user = new User(req.body)
+    user.save(function (err, user) {
+      if (err) return console.log(err)
+
+      res.status(200).json(user)
+    })
+  })
+
+  router.put('/user/:id', function (req, res) {
+    console.log(req.body)
+    let qry = { _id: req.params.id }
+  })
 }
