84607895e7ad5e3f6c747729cf6d8454b079dcb9
marb61a <marb61a@gmail.com>
2018-08-29 12:17:10 +0100

    added functionality to app.js

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 const express = require('express')
 const app = express()
+const api = require('./api')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
+app.set('port', process.env.PORT || 8081)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/api', api)
+app.use(express.static('static'))
+
+app.use(function (req, res, next) {
+  const err = new Error('Not found')
+  err.status = 404
+  res.json(err)
+})
+
+const mongoose = require('mongoose')
+mongoose.connect('')
